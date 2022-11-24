d35d79d68f261368e5fe9a9e8f939022fa078ecd
Keith Rowell <keith@keithrowell.com>
2022-11-25 07:36:10 +1000

    added face functionality

--- lib/toy_robot/game_board.rb
+++ lib/toy_robot/game_board.rb
@@ -1,21 +1,33 @@
 module ToyRobot
   class GameBoard
+    class InvalidFacing < ToyRobot::Error; end
 
     attr_reader :rows, :columns
     attr_reader :position
+    attr_reader :facing
 
     Position = Struct.new :x, :y
+    FACES = [:north, :east, :south, :west]
 
     def initialize rows, columns
       @rows = rows
       @columns = columns
       @position = Position.new 0, 0
+      @facing = :north
     end
 
     def place x, y
       if valid? x, y
         @position.x = x
         @position.y = y
+      end
+    end
+
+    def face direction
+      if FACES.include? direction
+        @facing = direction
+      else
+        raise InvalidFacing
       end
     end
 
