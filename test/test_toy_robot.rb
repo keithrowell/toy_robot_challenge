9d13323960d287aded28b453363515b64ff60914
Keith Rowell <keith@keithrowell.com>
2022-11-25 09:20:52 +1000

    added plament tests

--- test/test_toy_robot.rb
+++ test/test_toy_robot.rb
@@ -53,4 +53,26 @@ class TestToyRobot < Minitest::Test
       @robot.turn :invalid
     end
   end
+
+  def test_placement
+    @robot.place 2, 3, :south
+    assert_equal 2, @robot.position.x
+    assert_equal 3, @robot.position.y
+    assert_equal :south, @robot.facing
+
+    @robot.place 1, 4, :west
+    assert_equal 1, @robot.position.x
+    assert_equal 4, @robot.position.y
+    assert_equal :west, @robot.facing
+  end
+
+  def test_invalid_placement
+    assert_raises ToyRobot::Robot::InvalidCoordinate do
+      @robot.place 5, 5, :north
+    end
+    assert_raises ToyRobot::Robot::InvalidDirection do
+      @robot.place 2, 1, :invalid
+    end
+  end
+
 end
