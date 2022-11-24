04946e419f8a4a2163673ef97d43b5b78baef64a
Keith Rowell <keith@keithrowell.com>
2022-11-25 07:17:56 +1000

    added some basic game board tests

---
+++ test/test_game_board.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestGameBoard < Minitest::Test
+  def setup
+    @board = ToyRobot::GameBoard.new 5, 5
+  end
+
+  def test_that_it_has_rows_and_columns
+    assert_equal 5, @board.rows
+    assert_equal 5, @board.columns
+  end
+
+  def test_place
+    @board.place 2, 2
+    assert_equal 2, @board.position.x
+    assert_equal 2, @board.position.y
+  end
+
+  def test_valid
+    assert @board.valid? 2, 2
+    refute @board.valid? 5, 5
+    refute @board.valid? 3, 10
+    refute @board.valid? -1, 0
+  end
+
+  def test_invalid_place_does_not_change_position
+    @board.place 2, 3
+    @board.place 12, 2
+    assert_equal 2, @board.position.x
+    assert_equal 3, @board.position.y
+  end
+end
