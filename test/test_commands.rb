259b63218c2a3d5a83bb8395534ef7ac3d0241b8
Keith Rowell <keith@keithrowell.com>
2022-11-25 10:27:20 +1000

    added test for commands class

---
+++ test/test_commands.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestCommands < Minitest::Test
+  def setup
+    @commands = ToyRobot::Commands.new
+  end
+
+  def test_commands_without_place_are_ignored
+    @commands.move
+    assert_nil @commands.report
+    @commands.left
+    assert_nil @commands.report
+    @commands.right
+    assert_nil @commands.report
+    @commands.report
+    assert_nil @commands.report
+  end
+
+  def test_place
+    @commands.place 2, 3, :north
+    assert_equal "2, 3, NORTH", @commands.report
+  end
+
+  def test_move
+    @commands.place 2, 3, :north
+    @commands.move
+    assert_equal "2, 4, NORTH", @commands.report
+  end
+
+  def test_left
+    @commands.place 2, 3, :north
+    @commands.left
+    assert_equal "2, 3, WEST", @commands.report
+  end
+
+  def test_right
+    @commands.place 2, 3, :north
+    @commands.right
+    assert_equal "2, 3, EAST", @commands.report
+  end
+end
